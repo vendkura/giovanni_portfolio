470e8e1ea1ce0a1fdbae1adc008307a405209d11
Ashèlèyiné <51537350+vendkura@users.noreply.github.com>
2025-06-07 19:53:05 +0700

    Update previous website with Nextjs template

---
+++ app/config.ts
@@ -0,0 +1,16 @@
+export const metaData = {
+  baseUrl: "https://nextfolio-template.vercel.app/",
+  title: "GIOVANNI",
+  name: "Ibsen Giovanni BAZIE",
+  ogImage: "/opengraph-image.png",
+  description:
+    "Software and Media Engineer.",
+};
+
+export const socialLinks = {
+  // twitter: "https://x.com/1tssirius",
+  github: "https://www.github.com/vendkura",
+  // instagram: "https://www.instagram.com/",
+  linkedin: "https://www.linkedin.com/bazieibsengiovanni",
+  email: "mailto:bazieibsen@yahoo.com",
+};
